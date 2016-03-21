20285f485c19bd0dcac9e8e0f8cac99ddd538918
Wolnewitz <wolnewitz@gmail.com>
2016-03-21 16:26:13 -0400

    Extract User follower logic into Following concern

---
+++ app/models/concerns/following.rb
@@ -0,0 +1,27 @@
+module Following
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :followed_relationships,
+      foreign_key: :follower_id,
+      class_name: FollowingRelationship
+    has_many :followed_users, through: :followed_relationships
+
+    has_many :follower_relationships,
+      foreign_key: :followed_user_id,
+      class_name: FollowingRelationship
+    has_many :followers, through: :follower_relationships
+
+    def follow user
+      followed_users << user
+    end
+
+    def unfollow user
+      followed_users.delete(user)
+    end
+
+    def following? user
+      user.followers.include? self
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,7 @@
 class User < ActiveRecord::Base
+  include Following
   has_many :shouts
-
-  has_many :followed_relationships,
-    foreign_key: :follower_id,
-    class_name: FollowingRelationship
-  has_many :followed_users, through: :followed_relationships
-
-  has_many :follower_relationships,
-    foreign_key: :followed_user_id,
-    class_name: FollowingRelationship
-  has_many :followers, through: :follower_relationships
 
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
-
-  def follow user
-    followed_users << user
-  end
-
-  def unfollow user
-    followed_users.delete(user)
-  end
-
-  def following? user
-    user.followers.include? self
-  end
 end
