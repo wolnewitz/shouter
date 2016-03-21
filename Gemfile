6e308c5681839e8a2b3747003eb17a0310273fc3
Wolnewitz <wolnewitz@gmail.com>
2016-03-21 10:37:55 -0400

    added polymorphism to shout

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 
 group :development, :test do
+  gem 'pry-rails'
   gem 'byebug'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :show,  :create]
-  resources :shouts, only: [:create, :show]
+  resources :shouts, only: [:show]
+  resources :text_shouts, only: [:create]
 end
