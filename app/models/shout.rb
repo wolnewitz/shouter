11aae96f8679defe2c5ebf85b403ef6cc7c620c1
Wolnewitz <wolnewitz@gmail.com>
2016-03-23 14:38:22 -0400

    Refactor search

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -5,8 +5,7 @@ class Shout < ActiveRecord::Base
 
   validates_associated :content
 
-  def self.search term
-    text_shouts = TextShout.where("body LIKE ?", "%#{term}%")
-    where(content_type: 'TextShout', content_id: text_shouts)
+  def self.text_shouts
+    where("content_type = ?", "TextShout")
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   include Following
-  has_many :shouts
+  has_many :shouts, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
